c82d8bc02c4bcef6d0c29636be0120ca946aa647
nessime7 <sara.przebinda@gmail.com>
2022-11-22 23:21:09 +0100

    version 1

---
+++ src/main/resources/db/migration/V1__create_accoounts.sql
@@ -0,0 +1,9 @@
+create TABLE IF NOT EXISTS accounts
+(
+    id UUID default gen_random_uuid(),
+    name CHARACTER VARYING(30) NOT NULL,
+    balance DECIMAL(1000,2) NOT NULL,
+    currency CHARACTER VARYING(4) NOT null,
+    CONSTRAINT accounts_pkey PRIMARY KEY (id)
+);
+-> 12152151
---
+++ src/main/resources/db/migration/V4__insert_to_cards.sql
@@ -0,0 +1,4 @@
+insert into cards (id,account_id,name,surname,max_limit,max_debet,card_type) values ('ac3a7a5a-1949-4182-b2ca-7f138bd19915','fc35ad28-91fc-449b-af3e-918417266f9d','Sara','Przebinda',100.0,100.0,'CREDIT');
+insert into cards (id,account_id,name,surname,max_limit,max_debet,card_type) values ('71781a31-ed7f-4127-9734-aa4c23cb74a7','fc35ad28-91fc-449b-af3e-918417266f9d','Sara','Przebinda',100.0,100.0,'PAYMENT');
+insert into cards (id,account_id,name,surname,max_limit,max_debet,card_type) values ('5e6e4432-1723-4e77-80d5-70cf1f83c80a','5f73cec7-6ac1-46ee-a203-794c35d8800c','Albert','Podraza',100.0,100.0,'PAYMENT');
+insert into cards (id,account_id,name,surname,max_limit,max_debet,card_type) values ('bd8bcbc5-0a0b-47d0-8af9-2afc824a3a96','5f73cec7-6ac1-46ee-a203-794c35d8800c','Albert','Podraza',100.0,100.0,'CREDIT');
